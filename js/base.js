56b920038580635918dbc4ea1ff3f3038c093904
sjzhenglijun <zlj@zljdeMacBook-Pro.local>
2017-02-24 22:16:45 +0800

    2.03
    调整

--- js/base.js
+++ js/base.js
@@ -1,15 +1,29 @@
 /**
  * Created by zlj on 17/2/18.
  */
-$(function()
-{
-    //    禁止鼠标拖动img
-    $('#noDrag').mousedown(function(){return false});
+//    禁止鼠标拖动img
+$('#noDrag').mousedown(function () {
+    return false
+});
 
-    //砖石插件
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 5
-    });
+//砖石插件
 
-})
+// external js: masonry.pkgd.js
+
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    columnWidth: 100,
+    stagger: 20,
+});
+
+grid.addEventListener( 'click', function( event ) {
+    // don't proceed if item was not clicked on
+    if ( !matchesSelector( event.target, '.grid-item' ) ) {
+        return;
+    }
+    // change size of item via class
+    event.target.classList.toggle('grid-item--gigante');
+    // trigger layout
+    msnry.layout();
+});
